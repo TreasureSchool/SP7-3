40d52d1b028b50e04b9e4465792e5e2f566a3410
PeterHenriksen <peter_vejen_henriksen@hotmail.com>
2016-09-09 00:42:09 +0200

    Add files via upload
    done deal

---
+++ echo/src/main/java/server/ClientHandler.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import shared.ProtocolStrings;
+
+/**
+ *
+ * @author jens
+ */
+public class ClientHandler implements Runnable {
+
+    private final Socket socket;
+    private final PrintWriter writer;
+    private final EchoServer server;
+    private final Scanner input;
+    private String name;
+    public boolean loggedIn = false;
+
+    public ClientHandler(Socket socket, Scanner input, PrintWriter writer, EchoServer server) {
+        this.socket = socket;
+        this.writer = writer;
+        this.server = server;
+        this.input = input;
+    }
+
+    public static ClientHandler handle(Socket socket, EchoServer server) throws IOException {
+        Scanner input = new Scanner(socket.getInputStream());
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        return new ClientHandler(socket, input, writer, server);
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                String message = input.nextLine();
+                String[] msg = message.split(":");
+                System.out.println("Received: " + message);
+
+                if (!loggedIn) {
+                    if (msg[0].equals(ProtocolStrings.LOGIN)) {
+                        loggedIn = true;
+                        name = msg[1];
+                        printClientList();
+                    }
+                } else {
+
+                    switch (msg[0]) {
+                        case "MSG":
+                            if (msg[0].equals(ProtocolStrings.MSG) && (msg[1].equals(""))) {
+                                server.sendMulticast(ProtocolStrings.MSGRES + msg[2]);
+                            } else {
+                                String[] users = msg[1].split(",");
+                                for (ClientHandler client : server.getClientHandlers()) {
+                                    for (String user : users) {
+                                        if(client.getName().equals(user)){
+                                            client.sendMessage(ProtocolStrings.MSGRES + msg[2]);
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                        case "LOGOUT":
+                            if (msg[0].equals(ProtocolStrings.LOGOUT)) {
+                                try {
+                                    writer.println(ProtocolStrings.LOGOUT);//Echo the stop message back to the client for a nice closedown
+                                    socket.close();
+
+                                } catch (IOException ex) {
+                                    Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }
+                            break;
+                        default: { 
+                    System.out.println("    default    ");
+                    }
+                    
+                }
+                }
+                
+            }
+
+        } finally {
+            try {
+               writer.println(ProtocolStrings.LOGOUT);//Echo the stop message back to the client for a nice closedown
+               socket.close();
+               server.removeHandler(this);
+               printClientList();
+                System.out.println("Closed a Connection");
+            } catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+
+    public void sendMessage(String message) {
+        System.out.println("Sending " + message);
+        writer.println(message);
+        writer.flush();
+    }
+    
+    public void printClientList() {
+       ArrayList<String> clientList = new ArrayList<>();
+
+        for (ClientHandler c : server.getClientHandlers()) {
+            if (c.getName() != null)
+                clientList.add(c.getName());
+        }
+
+        server.sendMulticast("CLIENTLIST:" + String.join(",", clientList));
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    
+}
+
---
+++ echo/src/main/java/shared/ProtocolStrings.java
@@ -0,0 +1,9 @@
+package shared;
+
+public class ProtocolStrings {
+  public static final String LOGIN = "LOGIN";
+  public static final String LOGOUT = "LOGOUT";
+  public static final String MSG = "MSG";
+  public static final String MSGRES = "MSGRES";
+  public static final String CLIENTLIST = "CLIENTLIST";
+}
