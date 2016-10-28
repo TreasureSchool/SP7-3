58ebc1a331be9c2000b0819f726e5a208731f5aa
Joachim <Joachim@10.50.138.62>
2016-10-28 11:05:53 +0200

    Close to done-ish

--- SP7-EX3/src/main/webapp/app/myApp.js
+++ SP7-EX3/src/main/webapp/app/myApp.js
@@ -36,24 +36,38 @@ app.directive("formatCompany", function () {
     };
 });
 
-app.directive("imageInsertion", function() {
+app.directive("imageInsertion", function () {
     return {
         restrict: 'E',
-        templateUrl: 'image-insertion.html'
+        templateUrl: 'image-insertion.html',
+        controller: function ($scope, attrs) {
+            console.log(attrs.test);
+            var linkParts = attrs.content.split(',');
+            var imgLink;
+            $scope.images = [];
+            for (i = 0; i > linkParts.length; i++) {
+                imgLink = "pictures/" + linkParts[i];
+                $scope.images.push(imgLink);
+            }
+        },
+        controllerAs: "images"
     };
 });
 
-app.controller('RandomController', function($scope) {
-    
+app.controller('RandomController', function ($scope, RandomFactory) {
+    $scope.randomNumber = RandomFactory.getRandomNumber(6);
+    $scope.randomString = RandomFactory.getRandomString(5);
 });
 
 app.factory('RandomFactory', function () {
     var getRandomNumber = function (n) {
-        
+        min = Math.ceil(1);
+        max = Math.floor(n);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    
+
     var getRandomString = function (n) {
-        
+        return Array(n + 1).join((Math.random().toString(36) + '00000000000000000').slice(2, 18)).slice(0, n);
     };
     return {
         getRandomNumber: getRandomNumber,
@@ -61,22 +75,26 @@ app.factory('RandomFactory', function () {
     };
 });
 
-app.controller('CountryController', function() {
-    
+app.controller('CountryController', function ($scope, CountryFactory) {
+    $scope.allCountries = CountryFactory.getAllCountries();
+    $scope.allRegionCountries = CountryFactory.getAllCountriesFromRegion();
+    $scope.allCodeCountries = CountryFactory.getAllCountriesFromCode();
 });
-app.factory('CountryFactory', function() {
-    var getAllCountries = function() {
-        
+app.factory('CountryFactory', function ($http) {
+    
+    var urlBase = 'https://restcountries.eu/rest/v1';
+    var getAllCountries = function () {
+        return $http.get(urlBase + '/all');
     };
-    var getAllCountriesFromRegion = function() {
-        
+    var getAllCountriesFromRegion = function () {
+        return $http.get(urlBase + '/region/africa');
     };
-    var getAllCountriesFromCode = function() {
-        
+    var getAllCountriesFromCode = function () {
+        return $http.get(urlBase + '/alpha?codes=gb');
     };
     return {
-       getAllCountries: getAllCountries,
-       getAllCountriesFromRegion: getAllCountriesFromRegion,
-       getAllCountriesFromCode: getAllCountriesFromCode
+        getAllCountries: getAllCountries,
+        getAllCountriesFromRegion: getAllCountriesFromRegion,
+        getAllCountriesFromCode: getAllCountriesFromCode
     };
 });
